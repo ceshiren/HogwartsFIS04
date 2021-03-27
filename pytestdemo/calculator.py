89e1a8c30bdff4a33c55876e32dd576ea0970d18
xj418974188 <xujuan418974188@163.com>
2021-03-27 14:50:18 +0800

    pytest 第一节作业，浮点数，除数为0，异常情况的特殊处理，以及数据驱动的应用

--- pytestdemo/calculator.py
+++ pytestdemo/calculator.py
@@ -3,7 +3,6 @@ __author__ = 'hogwarts_xixi'
 __time__ = '2021/3/23 8:56 下午'
 """
 
-
 # 被测代码，实现计算器功能
 class Calculator:
     # 相加的功能
@@ -13,3 +12,6 @@ class Calculator:
     # 相减功能
     def sub(self, a, b):
         return a - b
+
+    def div(self, a, b):
+        return a / b
